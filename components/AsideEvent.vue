26b6bccf0ae19d5fdffc71bbbf1ba93b8be3c801
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-09-02 18:32:23 +0700

    Initial commit

---
+++ components/AsideEvent.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="el-detail">
+    <div class="el-detail_title">el-detail_title</div>
+    <div class="el-detail_name">
+      <el-card shadow="never">
+        Detail Name
+      </el-card>
+    </div>
+    <div class="el-detail_title">el-detail_date</div>
+    <div class="el-detail_date">
+      <el-card shadow="never">
+        Detail Date
+      </el-card>
+    </div>
+    <div class="el-detail_title">el-detail_description</div>
+    <div class="el-detail_description">
+      <el-card shadow="never">
+        Detail Description
+      </el-card>
+    </div>
+    <div class="el-detail_title">el-detail_label</div>
+    <div class="el-detail_label">
+      <el-tag>Tag One</el-tag>
+      <el-tag type="success">Tag Two</el-tag>
+      <el-tag type="info">Tag Three</el-tag>
+      <el-tag type="warning">Tag Four</el-tag>
+      <el-tag type="danger">Tag Five</el-tag>
+    </div>
+    <div class="el-detail_title">el-detail_location</div>
+    <div class="el-detail_location">
+      <el-card shadow="never">
+        Detail Location
+      </el-card>
+    </div>
+  </section>
+</template>
